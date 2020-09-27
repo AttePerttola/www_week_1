339c86276abaa0628a14dcdba7ee7270c27641aa
AttePerttola <57904504+AttePerttola@users.noreply.github.com>
2020-09-27 15:51:00 +0300

    table works

--- src/index.js
+++ src/index.js
@@ -14,23 +14,44 @@ if (document.readyState !== "loading") {
 
 function initializeCode() {
   console.log("Initializing");
-  const button = document.getElementById("start");
-  button.addEventListener("mousedown", (event) => {
-    renderBoard();
-    event.stopPropagation();
-  });
+
+  renderBoard();
 }
+
+let turn = "x";
+const x = "x";
+const o = "o";
+let tableBoard;
 
 function renderBoard() {
   //renders the 5x5 board
-  const tableBody = document.getElementById("tableData");
-  let dataHtml = "";
+
+  tableBoard = document.getElementById("tableBoard");
 
   for (let i = 0; i <= 4; i++) {
+    let row = document.createElement("tr");
+    tableBoard.append(row);
     for (let j = 0; j <= 4; j++) {
-      dataHtml += `<tr><td id=${("i", "j")}></td></tr>`;
+      let cell = document.createElement("td");
+      cell.onclick = function () {
+        if (cell.innerHTML !== "") {
+          return;
+        }
+        cell.innerHTML = turn;
+        checkWinner(cell, i, j);
+        turn = turn === x ? o : x;
+      };
+      row.append(cell);
     }
   }
-  console.log(dataHtml);
-  tableBody.innerHTML = dataHtml;
+}
+
+function checkWinner(cell, i, j) {
+  for (let k = -4; k <= 4; k++) {
+    let cell_i = i + k;
+    if (cell_i >= 0 && cell_i <= 4) {
+      const row = tableBoard.rows[cell_i];
+      console.log(row);
+    }
+  }
 }
